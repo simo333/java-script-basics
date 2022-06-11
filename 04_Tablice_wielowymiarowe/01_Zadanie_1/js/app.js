d35647ec341b172c75a9560e9290046f3095252a
simo333 <szymon.333@wp.pl>
2022-06-11 14:12:03 +0200

    04 Tablice wielowymiarowe

---
+++ 04_Tablice_wielowymiarowe/01_Zadanie_1/js/app.js
@@ -0,0 +1,11 @@
+const multiValueArray = [
+  [2, 3],
+  ["Ala", "Ola"],
+  [true, false],
+  [5, 6, 7, 8],
+  [12, 15, 67]
+];
+
+console.log(multiValueArray[3][2]);
+console.log(multiValueArray[1].length);
+console.log(multiValueArray[4][2]);
---
+++ 04_Tablice_wielowymiarowe/02_Zadanie_2/js/app.js
@@ -0,0 +1,17 @@
+function print2DArray(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            console.log(arr[i][j])
+        }
+    }
+}
+
+const multiValueArray = [
+    [2, 3],
+    ["Ala", "Ola"],
+    [true, false],
+    [5, 6, 7, 8],
+    [12, 15, 67]
+];
+
+print2DArray(multiValueArray);
