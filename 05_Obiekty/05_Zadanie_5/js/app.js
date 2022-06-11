4077dd27fcb27499950154a8af500918f7cbbd78
simo333 <szymon.333@wp.pl>
2022-06-11 14:53:56 +0200

    05 Obiekty - Zadanie 5

---
+++ 05_Obiekty/05_Zadanie_5/js/app.js
@@ -0,0 +1,10 @@
+const favRecipe = {
+    title: "Spaghetti carbonara",
+    servings: 6,
+    ingredients: ["makaron", "ser carbonara", "mięso"]
+}
+
+console.log(favRecipe);
+console.log(favRecipe.title);
+console.log(favRecipe.servings);
+console.log(favRecipe.ingredients);
