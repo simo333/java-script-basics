bee732bfe0f2cb3f19d47ac98fa7780eb8a1e7cc
simo333 <szymon.333@wp.pl>
2022-06-11 14:52:45 +0200

    05 Obiekty - Zadanie 1

---
+++ 05_Obiekty/01_Zadanie_1/js/app.js
@@ -0,0 +1,13 @@
+const country = {
+    capitol: "warszawa",
+    population: 36000000,
+    president: "duda",
+    ministers: [1, 2, 3]
+}
+
+console.log(
+    country.capitol + "\n",
+    country.population + "\n",
+    country.president + "\n",
+    country.ministers
+);
