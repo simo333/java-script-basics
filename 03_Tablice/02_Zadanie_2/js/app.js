97135a0264b47ef65e9d6cf937b66fe8d74f1656
simo333 <szymon.333@wp.pl>
2022-06-11 12:38:49 +0200

    03 Tablice - Zad 2

---
+++ 03_Tablice/02_Zadanie_2/js/app.js
@@ -0,0 +1,7 @@
+let arr = ["apple", "pineapple", "strawberry", "pear", "banana"];
+
+console.log(arr[0]);
+console.log(arr[arr.length - 1]);
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
