58850f629be29fcf6c15aede75416d454740e4a6
simo333 <szymon.333@wp.pl>
2022-06-11 12:39:12 +0200

    03 Tablice - Zad 3

---
+++ 03_Tablice/03_Zadanie_3/js/app.js
@@ -0,0 +1,14 @@
+function createArray(number) {
+  const newArray = [];
+
+  for (let counter = 1; counter <= number; counter++) {
+    newArray.push(counter);
+  }
+
+  return newArray;
+}
+
+console.log("tablica z liczbami do 6 = " + createArray(6));
+console.log("tablica z liczbami do 1 = " + createArray(1));
+console.log("Test dla liczby ujemnej (powinna być pusta tablica) " + createArray(-6));
+console.log("Test dla zera (powinna być pusta tablica) " + createArray(0));
