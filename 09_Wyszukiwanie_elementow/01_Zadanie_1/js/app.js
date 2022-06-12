7a2ec7ef0a72b6b4855839a1d1ff0071128f8ba4
simo333 <szymon.333@wp.pl>
2022-06-12 11:22:25 +0200

    09 Wyszukiwanie elementów - Zadania 1, 2

---
+++ 09_Wyszukiwanie_elementow/01_Zadanie_1/js/app.js
@@ -0,0 +1,10 @@
+const firsth1 = document.getElementsByTagName("h1")[0];
+const firsth1Query = document.querySelector("h1");
+console.log(firsth1);
+console.log(firsth1Query);
+
+const title = document.getElementsByClassName("title")[0];
+console.log(title);
+
+const dataAnim = document.querySelector("[data-animation]");
+console.log(dataAnim);
---
+++ 09_Wyszukiwanie_elementow/02_Zadanie_2/js/app.js
@@ -0,0 +1,8 @@
+const divs = document.querySelectorAll("div");
+console.log("Divs: " + divs.length);
+
+const classOffers = document.querySelectorAll(".offers");
+console.log(".offers: " + classOffers.length);
+
+const hrefAttr = document.querySelectorAll("[href]");
+console.log("hrefs: " + hrefAttr.length);
