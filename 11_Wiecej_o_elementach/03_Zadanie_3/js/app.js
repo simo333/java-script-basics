63029a40be787a3f7070a6b587af87142e6e0e87
simo333 <szymon.333@wp.pl>
2022-06-12 14:28:43 +0200

    11 Więcej o elementach - Zadanie 3

---
+++ 11_Wiecej_o_elementach/03_Zadanie_3/js/app.js
@@ -0,0 +1,10 @@
+/*
+  Poniżej napisz kod rozwiązujący zadania
+ */
+let name = document.getElementById("name");
+let favColor = document.getElementById("fav_color");
+let favMeal = document.getElementById("fav_meal");
+
+name.innerText += "Szymon";
+favColor.innerText += "niebieski";
+favMeal.innerText += "mięso";
