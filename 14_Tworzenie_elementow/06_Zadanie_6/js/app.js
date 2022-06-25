1cec07a5718caa9f44db07c4d322dbc9574ab9c3
simo333 <szymon.333@wp.pl>
2022-06-25 16:15:09 +0200

    14 Tworzenie nowych elementów - Zadanie 6

--- 14_Tworzenie_elementow/06_Zadanie_6/js/app.js
+++ 14_Tworzenie_elementow/06_Zadanie_6/js/app.js
@@ -1,19 +1,14 @@
+const items = document.querySelectorAll("a");
 const list1 = document.getElementById("list1");
 const list2 = document.getElementById("list2");
 
-list1.addEventListener("click", function (event) {
-    let element = event.target;
-    if(event.target.tagName === "STRONG") {
-        element = event.target.parentElement;
-    }
-    list2.appendChild(element);
-});
-
-list2.addEventListener("click", function (event) {
-    let element = event.target;
-    if(event.target.tagName === "STRONG") {
-        element = event.target.parentElement;
-    }
-    list1.appendChild(element);
-});
+items.forEach(function (item){
+    item.addEventListener("click", function (){
+        if (this.parentElement === list1){
+            list2.append(this);
+        } else {
+            list1.append(this);
+        }
+    })
+})
 
