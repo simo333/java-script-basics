6d82a92f69a4ac0d6679a10c83f38b8535d49406
simo333 <szymon.333@wp.pl>
2022-06-26 12:09:22 +0200

    16 Fetch - Zadanie 3

---
+++ 16_Fetch/03_Zadanie_3/js/app.js
@@ -0,0 +1,41 @@
+const mainDiv = document.getElementById("app");
+const select = document.querySelector("#selectCountry");
+
+function fetchHolidays() {
+    fetch("https://fer-api.coderslab.pl/v1/holidays?key=e92601251-c0a2-4s63-v73f-54041195480f&country="
+        + select.value)
+        .then(function (res) {
+            if(res.ok) {
+                return res.json();
+            }
+            throw new Error("Fetching failed.");
+        })
+        .then(function (data) {
+            console.log(data.holidays);
+            const ul = document.createElement("ul");
+            data.holidays.forEach(function (holiday) {
+                const li = document.createElement("li");
+                const h3Name = document.createElement("h3");
+                h3Name.classList.add("holiday-name");
+                h3Name.innerText = holiday.name;
+                const h3Date = document.createElement("h3");
+                h3Date.classList.add("holiday-date");
+                h3Date.innerText = holiday.date;
+                li.append(h3Name);
+                li.append(h3Date);
+                ul.append(li);
+            });
+            mainDiv.append(ul);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+
+select.addEventListener("change", function () {
+    mainDiv.innerText = "";
+    fetchHolidays();
+})
+
+fetchHolidays();
